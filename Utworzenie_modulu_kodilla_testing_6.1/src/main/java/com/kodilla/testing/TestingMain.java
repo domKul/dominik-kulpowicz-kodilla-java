b1e92f57de92a73c35a35dfb380990bd41be9c1a
Dominik Kulpowicz <dominik.kulpowicz@gmail.com>
2022-10-13 18:37:58 +0200

    6.2 adding testing package

--- Utworzenie_modulu_kodilla_testing_6.1/src/main/java/com/kodilla/testing/TestingMain.java
+++ Utworzenie_modulu_kodilla_testing_6.1/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,38 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.Calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+        String result = simpleUser.getUsername();
+
+        if(result.equals("theForumUser")){
+            System.out.println("Test OK");
+        }else{
+            System.out.println("Error!");
+        }
+
+      System.out.println("Test- Pierwszy test jednostkowy");
+        Calculator calculator = new Calculator();
+        int addresult = calculator.add(123, 123);
+        int substractresult = calculator.substract(123, 123);
+
+         System.out.println("Add result test of ");
+        if(addresult == 246){
+            System.out.println("Result of 123 + 123 are 246");
+        }else{
+            System.out.println("Error");
+        }
+        System.out.println("Substract result test");
+        if (substractresult ==(0)) {
+            System.out.println("result of 123 - 123  are 0");
+        }else{
+            System.out.println("Error");
+        }
+
+
+
     }
 }
