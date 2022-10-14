34af4faf52526725ff322ae947ec32b8f6ccc2ca
Dominik Kulpowicz <dominik.kulpowicz@gmail.com>
2022-10-14 09:59:22 +0200

    6.2 improvement of calculator

--- Utworzenie_modulu_kodilla_testing_6.1/src/main/java/com/kodilla/testing/Calculator/Calculator.java
+++ Utworzenie_modulu_kodilla_testing_6.1/src/main/java/com/kodilla/testing/Calculator/Calculator.java
@@ -1,13 +1,13 @@
 package com.kodilla.testing.Calculator;
 
 public class Calculator {
-    int a;
-    int b;
 
     public int add(int a, int b){
+
         return a + b;
     }
     public int substract(int a, int b){
+
         return a - b;
     }
 }
--- Utworzenie_modulu_kodilla_testing_6.1/src/main/java/com/kodilla/testing/TestingMain.java
+++ Utworzenie_modulu_kodilla_testing_6.1/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,9 @@
 package com.kodilla.testing;
 
-import com.kodilla.testing.Calculator.Calculator;
 import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.Calculator.Calculator;
+
+
 
 public class TestingMain {
     public static void main(String[] args) {
@@ -14,12 +16,13 @@ public class TestingMain {
             System.out.println("Error!");
         }
 
-      System.out.println("Test- Pierwszy test jednostkowy");
+        System.out.println("Test- Pierwszy test jednostkowy");
+
         Calculator calculator = new Calculator();
         int addresult = calculator.add(123, 123);
         int substractresult = calculator.substract(123, 123);
 
-         System.out.println("Add result test of ");
+        System.out.println("Add result test of ");
         if(addresult == 246){
             System.out.println("Result of 123 + 123 are 246");
         }else{
@@ -31,7 +34,6 @@ public class TestingMain {
         }else{
             System.out.println("Error");
         }
-
 
 
     }
