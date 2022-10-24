5dfc82bc2727a23aa5e570806fc4b9d1a750c63b
Dominik Kulpowicz <dominik.kulpowicz@gmail.com>
2022-10-24 03:42:05 +0200

    6.4 adding test of ShapeCollector class

---
+++ Utworzenie_modulu_kodilla_testing_6.1/src/test/java/com/kodilla/testing/shape/ShapeCollectorTestSuite.java
@@ -0,0 +1,98 @@
+package com.kodilla.testing.shape;
+
+import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
+
+public class ShapeCollectorTestSuite {
+    private static int testCounter = 0;
+
+@BeforeAll
+    public static void beforeAllTests(){
+    System.out.println("Begin of tests");
+}
+@AfterAll
+    public static void afterAllTests(){
+    System.out.println("All tests are done");
+}
+@BeforeEach
+    public void beforEachTest(){
+    testCounter++;
+    System.out.println("Preparing for execute test #" + testCounter);
+}
+
+    @Nested
+    @DisplayName("Adding and removing figures test")
+    class ShapeCollectionTestOne{
+
+    @Test
+    void testAddFigure(){
+        //Given
+        ShapeCollector shapeCollector = new ShapeCollector();
+        Circle circle = new Circle();
+        Square square = new Square();
+
+        //When
+        shapeCollector.addFigure(circle);
+        shapeCollector.addFigure(square);
+
+        //Then
+        Assertions.assertEquals(2, shapeCollector.figuresCollection.size());
+        Assertions.assertTrue(shapeCollector.figuresCollection.contains(circle));
+        Assertions.assertTrue(shapeCollector.figuresCollection.contains(square));
+    }
+
+    @Test
+    void testRemovingFigure(){
+        //Given
+        ShapeCollector shapeCollector = new ShapeCollector();
+        Circle circle= new Circle();
+        Square square = new Square();
+
+        //When
+        shapeCollector.addFigure(circle);
+        shapeCollector.addFigure(square);
+        shapeCollector.removeFigure(circle);
+
+        //Then
+        Assertions.assertTrue(shapeCollector.figuresCollection.contains(square));
+        Assertions.assertEquals(1, shapeCollector.figuresCollection.size());
+    }
+}
+   @Nested
+   @DisplayName("Testing how to show and get the firgures")
+   class ShapeCollectorTestTwo{
+    @Test
+    void testGetFigures(){
+        //Given
+        ShapeCollector shapeCollector = new ShapeCollector();
+        Circle circle = new Circle();
+
+        //When
+        shapeCollector.addFigure(circle);
+
+        //Then
+        Assertions.assertTrue(shapeCollector.figuresCollection.size()>0);
+        Assertions.assertEquals(1, shapeCollector.getFigure(1));
+
+    }
+    @Test
+    void testShowFigure(){
+        //Given
+        ShapeCollector shapeCollector = new ShapeCollector();
+        Circle circle = new Circle();
+        Square square = new Square();
+
+        //When
+        shapeCollector.addFigure(circle);
+        shapeCollector.addFigure(square);
+        shapeCollector.showFigures();
+
+        //Then
+        Assertions.assertEquals(2, shapeCollector.getFigure(2));
+
+    }
+
+}
+
+}
