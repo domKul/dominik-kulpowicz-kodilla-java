97979536626ff34faa0335b6865260b86b27d7b4
Dominik Kulpowicz <dominik.kulpowicz@gmail.com>
2022-11-01 15:54:46 +0100

    7.2 adding immutable class

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -2,6 +2,7 @@ package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
 import com.kodilla.stream.beautifier.PoemDecorator;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 
 public class StreamMain {
@@ -15,13 +16,18 @@ public class StreamMain {
         poemDecorator2.decorate("oOoOoO".toLowerCase());
         poemDecorator2.decorate("@".repeat(5));
 
-       */
-
         PoemBeautifier poemBeautifier = new PoemBeautifier();
         poemBeautifier.beautify("AAA", s -> System.out.println(s + " Test Text " + s));
         poemBeautifier.beautify("CCC".toLowerCase(), s -> System.out.println(s + " Test Text " + s));
         poemBeautifier.beautify("@".repeat(3), s -> System.out.println(s + " Test Text " + s));
         poemBeautifier.beautify("ili".toUpperCase(), s -> System.out.println(s + " Test Text " + s));
+
+       */
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
+
+
 
 
 
