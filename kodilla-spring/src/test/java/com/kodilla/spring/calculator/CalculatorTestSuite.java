359dbf8f9c34e48550df473dbdf0cf17b7529b0c
Dominik Kulpowicz <dominik.kulpowicz@gmail.com>
2023-01-15 13:34:38 +0100

    14.2 spring calculator

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -0,0 +1,34 @@
+package com.kodilla.spring.calculator;
+
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class CalculatorTestSuite {
+
+    @Autowired
+    private Calculator calculator;
+
+
+    @Test
+    void testCalculations(){
+        //Given
+        //When
+       double result1 = calculator.add(3.3,1.3);
+       double result2 =calculator.sub(3.3,3.3);
+       double result3 =calculator.div(15,3.0);
+       double result4 = calculator.mul(3.3,19.3);
+        //Then
+        assertEquals(4.6,result1);
+        assertEquals(0,result2);
+        assertEquals(5,result3);
+        assertEquals(63.69,result4);
+
+
+    }
+}
